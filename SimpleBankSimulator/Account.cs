1957fb19ec4ab6c9da496da4bcf1c9cdc5529ee5
deivisondev <deivisoneditordevideos@gmail.com>
2022-07-16 22:07:08 -0300

    Início

---
+++ SimpleBankSimulator/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace SimpleBankSimulator
+{
+    public class Account
+    {
+        public int Number { get; private set; }
+        public string Name { get; set; }
+        private double _balance;
+
+        public Account(int number, string name)
+        {
+            Number = number;
+            Name = name;
+        }
+
+        public Account(int number, string name, double initialDeposit) : this(number, name)
+        {
+            Deposit(initialDeposit);
+        }
+
+        public void Deposit(double value)
+        {
+            if (value > 0) _balance += value;
+            else Console.WriteLine("Valor inválido.");
+        }
+
+        public void Withdraw(double value)
+        {
+            if (value > 0) _balance -= value + 5.0;
+            else Console.WriteLine("Valor inválido.");
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Number}, Titular: {Name}, Saldo: $ {_balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ SimpleBankSimulator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace SimpleBankSimulator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Account account;
+
+            Console.Write("Entre o número da conta: ");
+            int number = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Entre com o titular da conta: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char initialDeposit = char.Parse(Console.ReadLine());
+
+            double value;
+
+            if (initialDeposit == 's' || initialDeposit == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account = new Account(number, name, value);
+
+            }
+            else
+            {
+                account = new Account(number, name);
+            }
+
+            Console.WriteLine($"\nDados da conta:\n{account}\n");
+
+            Console.Write("Entre um valor para depósito: ");
+            value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            account.Deposit(value);
+
+            Console.WriteLine($"Dados da conta atualizados:\n{account}\n");
+
+            Console.Write("Entre um valor para saque: ");
+            value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            account.Withdraw(value);
+
+            Console.WriteLine($"Dados da conta atualizados:\n{account}");
+        }
+    }
+}
